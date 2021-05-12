82707f31e7bf8c3e58264ed2be0ecdc5241c3d3b
DreamakerDimas <dart.dimas@gmail.com>
2021-05-12 17:11:16 +0300

    fix: node_modules, replicaSet initialization

--- start-dev.sh
+++ start-dev.sh
@@ -7,6 +7,11 @@
 
 started_at=$(date +"%s")
 
+echo "-----> Installing node_modules"
+mkdir -p ./server/node_modules
+npm install --prefix ./server/
+echo ""
+
 echo "-----> Provisioning containers"
 docker-compose --file docker-compose.yaml up -d
 echo ""
@@ -17,7 +22,16 @@ server_container=$(docker-compose --file docker-compose.yaml ps -q server-node)
 
 # Run Setup Relica Set
 echo "-----> Running Setup Relica Set"
-docker exec -it "$mongo_container" mongo --eval 'rs.initiate({_id : "rs0", members: [{ _id: 0, host: "127.0.0.1:27017", priority: 1 }]})'
+result=$(docker exec -it "$mongo_container" mongo --eval 'rs.initiate({_id : "rs0", members: [{ _id: 0, host: "127.0.0.1:27017", priority: 1 }]})')
+
+# Check if there are error, and repeat if is
+while [[ $result == *"Error"* ]]
+do
+echo "----- Error, trying again -----"
+result=$(docker exec -it "$mongo_container" mongo --eval 'rs.initiate({_id : "rs0", members: [{ _id: 0, host: "127.0.0.1:27017", priority: 1 }]})')
+done
+echo "${result}"
+
 echo ""
 
 echo "-----> Running Seeds"
